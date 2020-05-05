8de6435c41537a3694300d5ae36603db00079e01
Axel BARBIER <axel.barbier@epita.fr>
2020-05-05 16:10:49 +0200

    forbid multiple instances

---
+++ TankArcade/Assets/Scripts/MainMenu/CheckLaunched.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+public class CheckLaunched : MonoBehaviour
+{
+    public bool DisableScript = true;
+
+
+    void Start()
+    {
+        if (DisableScript)
+            return;
+
+        int run = PlayerPrefs.GetInt("isRunning", 0);
+        if (run == 1)
+            Application.Quit();
+        else
+            PlayerPrefs.SetInt("isRunning", 1);
+    }
+
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("isRunning", 0);
+    }
+}
