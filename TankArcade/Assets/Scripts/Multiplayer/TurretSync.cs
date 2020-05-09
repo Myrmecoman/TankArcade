718b66124bee788f31c1715e34b30580491ffdbb
Axel BARBIER <axel.barbier@epita.fr>
2020-05-09 23:48:35 +0200

    i

---
+++ TankArcade/Assets/Scripts/Multiplayer/TurretSync.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using UnityEngine;
+
+
+public class TurretSync : NetworkBehaviour
+{
+    public Transform turret;
+
+    private float updateValue = 0;
+
+
+    // update only every 0.5s
+    void Update()
+    {
+        updateValue += Time.deltaTime;
+        if (isLocalPlayer && updateValue >= 0.5f)
+        {
+            updateValue %= 0.5f;
+        }
+    }
+}
